7a9dca5a7f1a280778049ec580b6c74f85639a64
Web Ninja <x7y8tt@gmail.com>
2021-07-26 06:12:17 +0800

    Add Trust entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,31 +6,18 @@ import {
   TrustFundAdded,
   TrustReleased
 } from "../generated/DeTrust/DeTrust"
-import { ExampleEntity } from "../generated/schema"
+import { Trust } from "../generated/schema"
 
 export function handleTrustAdded(event: TrustAdded): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let trust = new Trust(event.params.trustId.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.name = event.params.name
-  entity.settlor = event.params.settlor
+  trust.name = event.params.name
+  trust.settlor = event.params.settlor
+  trust.beneficiary = event.params.beneficiary
+  trust.nextReleaseTime = event.params.startReleaseTime
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  trust.save()
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
